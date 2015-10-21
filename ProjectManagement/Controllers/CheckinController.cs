7148bd5729e0389129837348bea4dcd31bd14218
Vincent Hounshell <hounvs@gmail.com>
2015-10-20 20:12:44 -0400

    Initial commit. Add home page and set up project

---
+++ ProjectManagement/Controllers/CheckinController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectManagement.Controllers
+{
+    public class CheckinController : Controller
+    {
+        // GET: Checkin
+        public ActionResult Index(string company)
+        {
+            switch(company.ToLower())
+            {
+                case "moldpro":
+                    ViewBag.Title = "moldPro";
+                    break;
+                case "oths":
+                    ViewBag.Title = "Oak Tree Home Services";
+                    break;
+                case "otr":
+                    ViewBag.Title = "Oak Tree Residential";
+                    break;
+                default:
+                    return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        // GET: Checkin/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Checkin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Checkin/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Checkin/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Checkin/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Checkin/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Checkin/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ProjectManagement/Helpers/HtmlHelpers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace ProjectManagement.Helpers
+{
+    public static class HtmlHelpers
+    {
+        // Extension method
+        public static MvcHtmlString ActionImage(this HtmlHelper html, string action, string controllerName, object routeValues, string imagePath, object htmlAttributes)
+        {
+            var url = new UrlHelper(html.ViewContext.RequestContext);
+
+            // build the <img> tag
+            var imgBuilder = new TagBuilder("img");
+            imgBuilder.MergeAttribute("src", url.Content(imagePath));
+            var attributes = HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes);
+            foreach (var attr in attributes)
+            {
+                imgBuilder.MergeAttribute(attr.Key, attr.Value.ToString());
+            }
+            string imgHtml = imgBuilder.ToString(TagRenderMode.SelfClosing);
+
+            // build the <a> tag
+            var anchorBuilder = new TagBuilder("a");
+
+            anchorBuilder.MergeAttribute("href", url.Action(action, controllerName, routeValues));
+            anchorBuilder.InnerHtml = imgHtml; // include the <img> tag inside
+            string anchorHtml = anchorBuilder.ToString(TagRenderMode.Normal);
+
+            return MvcHtmlString.Create(anchorHtml);
+        }
+    }
+}
