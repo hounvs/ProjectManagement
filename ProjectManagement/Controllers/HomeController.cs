d92dfd2368c1b3d87488a46a1246b12f208df268
Robert Kendall <rkendall0717@gmail.com>
2015-11-02 18:17:38 -0500

    Updated 'Contact' Page
    updated contact info for support and Rebekah (address, phone, email,
    etc)

--- ProjectManagement/Controllers/HomeController.cs
+++ ProjectManagement/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace ProjectManagement.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact Us";
 
             return View();
         }
